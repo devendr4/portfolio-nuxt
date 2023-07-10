e53700aa9286dd455d3030dadf1e024eef2fe25e
devendr4 <angel.emo.altuve@gmail.com>
2023-07-09 23:45:10 -0400

    default dark theme

--- store/index.ts
+++ store/index.ts
@@ -2,7 +2,7 @@ import { defineStore } from "pinia";
 
 export const useRootStore = defineStore("root", {
   state: () => ({
-    darkTheme: false,
+    darkTheme: true,
   }),
   actions: {
     toggleTheme() {
